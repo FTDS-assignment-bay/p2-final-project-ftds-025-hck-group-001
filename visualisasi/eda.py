8012bc40a9a5187f2abb0f5704ea9c8e88d51da2
Rania Alyandra <hadyanyusuf@gmail.com>
2025-04-23 18:16:42 +0700

    230425 18.16 Ian Push

--- visualisasi/eda.py
+++ visualisasi/eda.py
@@ -7,61 +7,23 @@ from PIL import Image
 def run():
 
     #Title
-    st.title('Bank Marketing Classification')
-
-    #Image
-    image = Image.open("bank_portugal.jpg")
-    st.image(image)
+    st.title('Exploratory Data Analysis')
     
-    st.markdown('---')
-    st.write('## Exploratory Data Analysis')
-
     #Load data
     st.write('### Data Loading')
-    df = pd.read_csv('bank.csv', delimiter=';')
+    df = pd.read_csv('DatosPrueba_clean.csv')
     st.dataframe(df)
 
-    df = df.rename(columns = 
-        {'poutcome' : 'outcome',
-        'y' : 'subscribed'}
-    )
+    # Fraud vs Non-Fraud Distribution (Bar Chart)
+    st.subheader("📊 Fraud vs Non Fraud")
+    fig1, ax1 = plt.subplots(figsize=(8, 6))
+    sns.countplot(x='is_fraud', data=df, palette="viridis", order=[0, 1], ax=ax1)
+    ax1.set_title("Distribusi Fraud vs Non Fraud")
+    ax1.set_xlabel("Distribusi")
+    ax1.set_ylabel("Jumlah")
+    ax1.set_xticklabels(['Non Fraud', 'Fraud'])
+    st.pyplot(fig1)
 
-    # Age Distribution
-    st.write('### Age Distribution')
-    fig = plt.figure(figsize=(12,5))
-    sns.histplot(x=df['age'], bins=20, kde=True, color='blue')
-    st.pyplot(fig)
-
-    # Job Distribution
-    st.write('### Job Distribution')
-    fig = plt.figure(figsize=(12,10))
-    sns.countplot(y=df['job'], order=df['job'].value_counts().index, hue=df['job'], palette="viridis")
-    st.pyplot(fig)
-
-    # Marital Distribution
-    st.write('### Marital Distribution')
-    fig = plt.figure(figsize=(12,10))
-    sns.countplot(x=df['marital'], order=df['marital'].value_counts().index, hue=df['marital'], palette="viridis")
-    st.pyplot(fig)
-
-    # Education Distribution
-    st.write('### Education Distribution')
-    fig = plt.figure(figsize=(12,10))
-    sns.countplot(x=df['education'], order=df['education'].value_counts().index, hue=df['education'], palette="viridis")
-    st.pyplot(fig)
-
-    # Loan or Housing Loan
-    st.write('### Personal Loan Vs. Housing Loan')
-    option = st.selectbox('Pilih column : ', ['loan', 'housing'])
-    fig = plt.figure(figsize=(12,10))
-    sns.countplot(data=df, x=option, palette="pastel")
-    st.pyplot(fig)
-
-    # Target Class Distribution
-    st.write('### Target Class Distribution')
-    fig = plt.figure(figsize=(12,10))
-    sns.countplot(x=df['subscribed'], hue=df['subscribed'], palette="viridis")
-    st.pyplot(fig)
 
 if __name__ == '__main__':
     run()
