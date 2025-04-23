da9af4fa47f547a196c4ff0e38ea47771a564b80
thalibanallaam <thalibanallaam@gmail.com>
2025-04-23 16:01:07 +0700

    ETL done

--- ETL/finpro_compose/dags/Finpro_DAG.py
+++ ETL/finpro_compose/dags/Finpro_DAG.py
@@ -3,7 +3,8 @@
 Final Project
 
 This program is made to automate the process of loading
-and transforming data from PostgreSQL.
+raw data from PostgreSQL, cleaning it, and posting it
+to PostgreSQL.
 
 =========================================================
 '''
@@ -13,23 +14,17 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import pandas as pd
-import psycopg2
 from sqlalchemy import create_engine
 
 # 1. FETCH DATA
 def fetch():
     '''
-    This function is used to fetch data from PostgreSQL. The data later will be processed
+    This function is used to fetch data from PostgreSQL. The data will be processed
     in the next function.
     '''
-    # Start connection between psycopg2 and postgres
-    conn = psycopg2.connect(
-        dbname="finpro_hck_025",
-        user="postgres",
-        password="postgres",
-        host="postgres", 
-        port="5432"
-    )
+    # Create a SQLAlchemy engine for PostgreSQL
+    engine = create_engine('postgresql+psycopg2://postgres:postgres@postgres:5432/finpro_hck_025')
+    conn = engine.connect()
 
     # Find the designated data in postgres and save it in "df" variable
     df = pd.read_sql_query("SELECT * FROM table_finpro", conn)
@@ -46,43 +41,51 @@ def clean():
     This function is used to process the previously fetched data.
 
     Processes:
+    - Drop unnecessary columns, duplicates, and missing values
+    - Convert data types (object to datetime)
+    - Replace commas with dots and convert to numeric
+    - Strip spaces and replace underscores with spaces in categorical columns
+    - Delete data with 'fraud' string in the 'merchant' column
+    - Fix capitalization in "category" column
+    - Export the processed data to a new csv file
+
+    Output: Cleaned data in CSV format
 
     '''
     # Read the previously fetched data
     df = pd.read_csv("/opt/airflow/data/Finpro_data_raw.csv")
 
-    # Function to normalize data
-    df.drop(columns=['unnamed_0', 'unnamed_01'], inplace=True) # Drop the first column
-    df.drop_duplicates(inplace=True) # Drop duplicates 
-    df.dropna(inplace=True) # Drop duplicates   
+    # Drop unnecessary columns
+    df.drop(columns=['unnamed_0', 'unnamed_01'], inplace=True)
+
+    # Drop duplicates and missing values
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True) 
 
     # Convert data type to datetime
-    df["trans_date_trans_time"] = pd.to_datetime(df["trans_date_trans_time"])
-    df["dob"] = pd.to_datetime(df["dob"])
-    df["unix_time"] = pd.to_datetime(df["unix_time"], origin='unix', unit='s')
+    date_cols = ["trans_date_trans_time", "dob", "unix_time"]
+    for col in date_cols:
+        df[col] = pd.to_datetime(df[col], errors="coerce")
 
     # Replace commas with dots and convert to numeric
     comma_to_float_cols = ["amt", "lat", "long", "merch_lat", "merch_long"]
     for col in comma_to_float_cols:
         df[col] = df[col].str.replace(",", ".").astype(float)
 
-    # Delete rows with 'fraud' in the 'merchant' column
-    df['merchant'] = df['merchant'].str.replace("fraud", "")
-
-    # 4. Strip spaces and and replace underscores with spaces in categorical columns
+    # Strip spaces and and replace underscores with spaces in categorical columns
     cat_cols = ["merchant", "category", "first", "last", "gender", "street", "city", "state", "job"]
     for col in cat_cols:
         df[col] = df[col].str.strip()
         df[col] = df[col].str.replace("_", " ")
 
-    # Drop duplicates
-    df = df.drop_duplicates()
+    # Delete data with 'fraud' string in the 'merchant' column
+    df['merchant'] = df['merchant'].str.replace("fraud", "")
 
-    # Drop missing values
-    df = df.dropna()
+    # Fix capitalization in "category" column
+    df['category'] = df['category'].str.title()
 
     # Export the processed data to a new csv file
-    df.to_csv("/opt/airflow/data/Finpro_data_clean.csv", index=False)
+    df.to_csv("/opt/airflow/data/Finpro_data_clean.csv", index=False, date_format="%Y-%m-%d %H:%M:%S")
 
 # 3. POST TO POSTGRESQL
 def post():
@@ -95,15 +98,18 @@ def post():
     conn = engine.connect()
 
     # Read the data we want to send
-    df = pd.read_csv("/opt/airflow/data/Finpro_data_clean.csv")
+    df = pd.read_csv("/opt/airflow/data/Finpro_data_clean.csv", parse_dates=["trans_date_trans_time", "dob", "unix_time"])
 
     # Send the data to PostgreSQL
     df.to_sql('table_finpro_cleaned', conn, if_exists='replace', index=False)
 
+    # Close connection
+    conn.close()
+
 # Define DAG
 default_args = {
     'owner': 'thaliban',
-    'start_date': datetime(2024, 4, 23),
+    'start_date': datetime(2025, 4, 21),
     'retries': 1
 }
 
@@ -116,7 +122,6 @@ with DAG(
     description="Fetch -> Clean -> Post") as dag:
 
     # Define operations within the DAG
-
     # Fetch Data
     fetch_data = PythonOperator(
         task_id='fetch_from_postgresql',
